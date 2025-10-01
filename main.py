2b1854414bd07d42dff3bc6215cf6cd33ae1e15d
Cr-Israel <carlosisrael08@hotmail.com>
2025-10-01 00:22:25 -0300

    feat: Add the Front-End URL

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    # "http://meusite.com",
+    "https://email-classifier-auto.netlify.app",
 ]
 
 app.add_middleware(
