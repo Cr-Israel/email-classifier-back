96851a119b093991901fea7075998ef785510806
Cr-Israel <carlosisrael08@hotmail.com>
2025-10-01 00:10:01 -0300

    feat: Add PORT

--- main.py
+++ main.py
@@ -24,3 +24,7 @@ def read_root():
     return {"Hello": "World"}
 
 app.include_router(email_router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
